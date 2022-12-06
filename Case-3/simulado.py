1647086c380d69cf4ea5f96e69b16e00fd5cf91c
Gustavo Matos <gu_costa_matos@hotmail.com>
2022-12-06 18:19:26 -0300

    add case3 arquivos

---
+++ Case-3/simulado.py
@@ -0,0 +1,10 @@
+'''
+1 - Média de acertos geral (em % e valor absoluto).
+2 -  Média de acertos de cada cada aluno (em % e valor absoluto)
+3 - Total de acertos de cada aluno
+
+'''
+
+
+def corretor():
+    pass
